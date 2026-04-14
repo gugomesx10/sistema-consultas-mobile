b30e9624ccab1f2c59c079887b15235756cb91cc
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-04-14 15:46:49 -0300

    refactor: extrair styles do HomeScreen para arquivo dedicado
    - Criado src/styles/home.styles.ts com export default
    - Removido StyleSheet.create inline do HomeScreen.tsx
    - Import atualizado para usar styles do arquivo externo

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,7 +4,6 @@ import {
   Text,
   ScrollView,
   TouchableOpacity,
-  StyleSheet,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useNavigation } from "@react-navigation/native";
@@ -12,6 +11,7 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/types";
 import { consultasIniciais } from "../data/consultasData";
 import { calcularFaturamento } from "../utils/consultaFunctions";
+import styles from "../styles/home.styles";
 
 type HomeNav = NativeStackNavigationProp<RootStackParamList, "Home">;
 
@@ -91,90 +91,3 @@ export default function HomeScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#79059C",
-  },
-  scrollContent: {
-    padding: 20,
-    paddingTop: 60,
-    paddingBottom: 40,
-  },
-  header: {
-    alignItems: "center",
-    marginBottom: 24,
-  },
-  titulo: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#fff",
-    marginBottom: 8,
-  },
-  subtitulo: {
-    fontSize: 18,
-    color: "#fff",
-    opacity: 0.9,
-  },
-  faturamentoCard: {
-    backgroundColor: "rgba(255, 255, 255, 0.15)",
-    borderRadius: 12,
-    padding: 16,
-    marginBottom: 16,
-    alignItems: "center",
-  },
-  faturamentoLabel: {
-    fontSize: 14,
-    color: "#fff",
-    opacity: 0.9,
-    marginBottom: 4,
-  },
-  faturamentoValor: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#fff",
-    marginBottom: 4,
-  },
-  faturamentoInfo: {
-    fontSize: 12,
-    color: "#fff",
-    opacity: 0.7,
-  },
-  statsRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 24,
-  },
-  statCard: {
-    backgroundColor: "rgba(255, 255, 255, 0.15)",
-    borderRadius: 12,
-    padding: 16,
-    alignItems: "center",
-    flex: 1,
-    marginHorizontal: 4,
-  },
-  statNumero: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#fff",
-  },
-  statLabel: {
-    fontSize: 12,
-    color: "#fff",
-    opacity: 0.8,
-    marginTop: 4,
-  },
-  menuBotao: {
-    backgroundColor: "#fff",
-    borderRadius: 12,
-    padding: 18,
-    marginBottom: 12,
-    alignItems: "center",
-  },
-  menuBotaoTexto: {
-    fontSize: 16,
-    fontWeight: "600",
-    color: "#79059C",
-  },
-});
